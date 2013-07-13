907f8d3ba9bf51503433dc4b69bc70325e6c4633
Jonathan Ellis <jbellis@apache.org>
2013-07-13 16:00:24 -0500

    r/m unnecessary checks in needsCleanup

--- src/java/org/apache/cassandra/db/compaction/CompactionManager.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionManager.java
@@ -461,18 +461,15 @@ public class CompactionManager implements CompactionManagerMBean
      */
     static boolean needsCleanup(SSTableReader sstable, Collection<Range<Token>> ownedRanges)
     {
-        if (ownedRanges.isEmpty())
-            // we shouldn't have an sstable at all
-            return true;
+        assert !ownedRanges.isEmpty(); // cleanup checks for this
 
         // unwrap and sort the ranges by LHS token
-        ArrayList<Range<Token>> sortedRanges = new ArrayList(Range.normalize(ownedRanges));
+        List<Range<Token>> sortedRanges = Range.normalize(ownedRanges);
 
         // see if there are any keys LTE the token for the start of the first range
         // (token range ownership is exclusive on the LHS.)
         Range<Token> firstRange = sortedRanges.get(0);
         if (sstable.first.token.compareTo(firstRange.left) <= 0)
-            // the first key falls before the first range
             return true;
 
         // then, iterate over all owned ranges and see if the next key beyond the end of the owned
@@ -481,23 +478,33 @@ public class CompactionManager implements CompactionManagerMBean
         {
             Range<Token> range = sortedRanges.get(i);
             if (range.right.isMinimum())
+            {
+                // we split a wrapping range and this is the second half.
                 // there can't be any keys beyond this (and this is the last range)
                 return false;
+            }
 
             DecoratedKey firstBeyondRange = sstable.firstKeyBeyond(range.right.maxKeyBound());
             if (firstBeyondRange == null)
+            {
                 // we ran off the end of the sstable looking for the next key; we don't need to check any more ranges
                 return false;
+            }
 
             if (i == (ownedRanges.size() - 1))
+            {
                 // we're at the last range and we found a key beyond the end of the range
                 return true;
+            }
 
             Range<Token> nextRange = sortedRanges.get(i + 1);
             if (!nextRange.contains(firstBeyondRange.token))
+            {
                 // we found a key in between the owned ranges
                 return true;
+            }
         }
+
         return false;
     }
 
