46d8acba35269dc8e37a10e1c366fb4b7ad0de6d
Tyler Hobbs <tyler@datastax.com>
2014-02-13 12:03:23 -0600

    Remove unused import

--- src/java/org/apache/cassandra/io/sstable/IndexSummary.java
+++ src/java/org/apache/cassandra/io/sstable/IndexSummary.java
@@ -23,7 +23,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
-import org.apache.cassandra.config.CFMetaData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
