8a9be6604bb3b49c9128c191c4f6b14be868ec0d
Tyler Hobbs <tyler@datastax.com>
2015-01-07 17:50:17 -0600

    Fix DISTINCT queries with paging or limits

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -2093,6 +2093,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
         List<Row> rows = new ArrayList<Row>();
         int columnsCount = 0;
         int total = 0, matched = 0;
+        boolean ignoreTombstonedPartitions = filter.ignoreTombstonedPartitions();
 
         try
         {
@@ -2127,8 +2128,11 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
                     removeDroppedColumns(data);
                 }
 
-                rows.add(new Row(rawRow.key, data));
-                matched++;
+                if (!ignoreTombstonedPartitions || !data.isMarkedForDelete())
+                {
+                    rows.add(new Row(rawRow.key, data));
+                    matched++;
+                }
 
                 if (data != null)
                     columnsCount += filter.lastCounted(data);
